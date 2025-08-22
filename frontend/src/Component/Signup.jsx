43c05f702c8f1a6925ec683143dc5e2fdbb711ed
Sovan Bera <berasovan618@gmail.com>
2025-08-22 21:50:45 +0530

    updated backend config with MongoDB connection

--- frontend/src/Component/Signup.jsx
+++ frontend/src/Component/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
 			password: data.password
 		};
 
-		await axios.post("http://localhost:4001/user/signup", UserInfo)
+		await axios.post(`${import.meta.env.VITE_API_URL}/user/signup`, UserInfo)
 			.then(res => {
 				console.log("User data from signup cmp if pass", res.data);
 				if (res.data) {
