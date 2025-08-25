3081afec99cb350517c819ff82c0c0a41ed64a4d
Sovan Bera <berasovan618@gmail.com>
2025-08-25 10:53:55 +0530

    Updated component

--- frontend/src/Component/Course.jsx
+++ frontend/src/Component/Course.jsx
@@ -10,7 +10,7 @@ function Course() {
 	useEffect(()=>{
 		const getBook=async()=>{
 			try{
-				const res=await axios.get("http://localhost:4001/book");
+				const res = await axios.get("https://bookstoore-2.onrender.com/book");
 				console.log(res.data);
 				setBook(res.data)
 			}
